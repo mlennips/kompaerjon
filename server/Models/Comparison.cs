0e15720663ea1c1a6a90cb39ad9395ac38b46342
mlennips <matthiaslennips@hotmail.com>
2022-10-11 16:41:18 +0200

    cleanup

--- server/Models/Comparison.cs
+++ server/Models/Comparison.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace KompaerjonApi.Models
 {
     public class Comparison
     {
+        [Key]
         public Guid Id { get; set; }
 
         //public User user = models.ForeignKey(User, on_delete: models.CASCADE);
@@ -9,11 +13,22 @@ namespace KompaerjonApi.Models
         public DateTime Created { get; set; }
 
         public DateTime Updated { get; set; }
-
+            
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Required]
         public string SearchScheme { get; set; }
+
+        [ForeignKey("Attributes")]
+        public virtual ICollection<ComparisonAttribute> Attributes { get; set; }
+
+        [ForeignKey("Entries")]
+        public virtual ICollection<ComparisonEntry> Entries { get; set; }
+
+        [ForeignKey("Shares")]
+        public virtual ICollection<ComparisonShare> Shares { get; set; }
     }
 }
