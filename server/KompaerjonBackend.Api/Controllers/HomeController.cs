ead785cbf8c90c307a0e6b44245cbbdb5605aa3a
mlennips <matthiaslennips@hotmail.com>
2022-10-12 11:35:13 +0200

    HomeController

---
+++ server/KompaerjonBackend.Api/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KompaerjonBackend.Business.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KompaerjonBackend.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Hello.");
+        }
+    }
+}
