bb738a4230199d32fb9a4cb7e24999bc3adf19ad
mlennips <matthiaslennips@hotmail.com>
2022-10-12 14:52:46 +0200

    JWT

---
+++ server/KompaerjonBackend.Infrastructure/Auth/JwtHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace KompaerjonBackend.Infrastructure.Auth
+{
+    public static class JwtHelper
+    {
+        public static JwtSecurityToken CreateToken(IConfiguration configuration,Guid userId, string email)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, email),
+                new Claim(ClaimTypes.Email, email),
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:Issuer"],
+                audience: configuration["JWT:Audience"],
+                expires: DateTime.Now.AddHours(12),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return token;
+        }
+    }
+}
+
