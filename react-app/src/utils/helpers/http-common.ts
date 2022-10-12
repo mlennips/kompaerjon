7b18b86c007e6048448f43ef58e16e9716c2fdb8
mlennips <matthiaslennips@hotmail.com>
2022-10-12 17:32:18 +0200

    Login und Token

--- react-app/src/utils/helpers/http-common.ts
+++ react-app/src/utils/helpers/http-common.ts
@@ -2,13 +2,13 @@
 
 import axios from 'axios';
 import AxiosMockAdapter from 'axios-mock-adapter';
-import LocalStorageService from '../../services/LocalStorageService';
+import AuthService from '../../services/AuthService';
 const axiosMockInstance = axios.create();
 const axiosLiveInstance = axios.create({
   baseURL: "http://localhost:27619/api",
   headers: {
     "Content-Type": "application/json",
-    "Authorization" : 'Bearer ' + LocalStorageService.get('token') 
+    "Authorization" : 'Bearer ' + AuthService.getToken()
   }
 });
 export const axiosMockAdapterInstance = new AxiosMockAdapter(axiosMockInstance, { delayResponse: 250 });
